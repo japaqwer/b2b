7e683162c3684ef218cdc41368e99b51a188eaeb
japaqwer <aktanjapa@gmail.com>
2025-12-08 17:31:07 +0600

    sdfgsd

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import s from "./Header.module.scss";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <div className={s.header}>
-      <img
-        src="https://s3.eu-north-1.amazonaws.com/exclusivenedbucket/b1fe3967-a65c-4aa2-80a7-e571ac46258e.png"
-        alt="ss"
-      />
+      <Link href={"/"}>
+        <img
+          src="https://s3.eu-north-1.amazonaws.com/exclusivenedbucket/b1fe3967-a65c-4aa2-80a7-e571ac46258e.png"
+          alt="ss"
+        />
+      </Link>
     </div>
   );
 }
